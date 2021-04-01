55b341302e4540d716b045e8c011ea770ac44f76
Tomasz Cekało <tomasz.cekalo@gmail.com>
2021-04-01 16:12:37 +0200

    Fighter parsing

---
+++ SherdogComScraper/Birthplace.cs
@@ -0,0 +1,9 @@
+﻿namespace SherdogComScraper
+{
+    public class Birthplace
+    {
+        public string Flag { get; set; }
+        public string Locality { get; set; }
+        public string Nationality { get; set; }
+    }
+}
---
+++ SherdogComScraper/Fighter.cs
@@ -0,0 +1,19 @@
+﻿namespace SherdogComScraper
+{
+    public class Fighter
+    {
+        public string ImageSrc { get; set; }
+        public string BirthDate { get; set; }
+        public string Age { get; set; }
+        public Birthplace Birthplace { get; set; }
+        public string Height { get; set; }
+        public string HeightMetric { get; set; }
+        public string Weight { get; set; }
+        public string WeightMetric { get; set; }
+        public string Association { get; set; }
+        public string WeightClass { get; set; }
+        //public string Wins { get; set; }
+        //public string Losses { get; set; }
+        //public string Draws { get; set; }
+    }
+}
